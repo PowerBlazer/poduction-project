22697439fe9b66ae2543330c876be5d63e57badb
root <root@1296593-cb25874.tw1.ru>
2023-03-05 21:13:38 +0300

    Css Configuration

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,29 @@
 import { RuleSetRule } from "webpack";
 import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildLoaders(options:BuildOptions):RuleSetRule[]{
+    const cssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+          options.isDev ? 'style-loader':MiniCssExtractPlugin.loader,
+          {
+            loader:'css-loader',
+            options:{
+                esModule: true,
+                modules:{
+                    auto:(resPath:string)=>Boolean(resPath.includes('.module.')),
+                    localIdentName:options.isDev 
+                    ? '[path][name]__[local]--[hash:base64:5]'
+                    : '[hash:base64:5]'
+                },
+                
+            }
+          },
+          "sass-loader",
+        ],
+    };
+
     const typescriptLoader = {
         test:/\.tsx?$/,
         use:"ts-loader",
@@ -10,5 +32,6 @@ export function buildLoaders(options:BuildOptions):RuleSetRule[]{
 
     return [
         typescriptLoader,
+        cssLoaders
     ]
 }
--- config/build/buildPlugin.ts
+++ config/build/buildPlugin.ts
@@ -1,4 +1,5 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { WebpackPluginInstance } from "webpack";
 import { BuildOptions } from "./types/config";
 
@@ -7,5 +8,9 @@ export function buildPlugins({paths}:BuildOptions):WebpackPluginInstance[]{
         new HtmlWebpackPlugin({
             template:paths.html
         }),
+        new MiniCssExtractPlugin({
+            filename:'css/[name].[contenthash:8].css',
+            chunkFilename:'css/[name].[contenthash:8].css'
+        })
     ]
 }
